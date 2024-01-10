d76e0f2b86cc4911e803072da9c53799ba5ab824
baglana <102351104+baglana@users.noreply.github.com>
2024-01-10 22:41:47 +0600

    Add a button and binds for inputs

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,10 @@
   let number = '';
   let month = 1;
   let year = 24;
+  
+  function handleClick() {
+    console.log("hello");
+  }
 </script>
 
 0000 0000 0000 0000
@@ -14,23 +18,27 @@ Jane Appleseed
 <form>
   <div>
     <label for="name">Cardholder Name</label>
-    <input type="text" name="name" id="name"/>e.g. Jane Appleseed
+    <input type="text" name="name" id="name" bind:value={name}/>e.g. Jane Appleseed
   </div>
   <div>
     <label for="number">Card Number</label>
-    <input type="text" name="number" id="number"/>e.g. 1234 5678 9123 0000
+    <input type="text" name="number" id="number" bind:value={number}/>e.g. 1234 5678 9123 0000
   </div>
   <div>
-    <label for="date">Exp. Date (MM/YY)</label>
-    <input type="number" name="month" id="month"/>MM
-    <input type="number" name="year" id="year"/>YY
+    <label>Exp. Date (MM/YY)
+      <input type="number" name="month" id="month" bind:value={month}/>MM
+      <input type="number" name="year" id="year" bind:value={year}/>YY
+    </label>
   </div>
   <div>
     <label for="cvc">CVC</label>
     <input type="number" name="cvc" id="cvc">e.g. 123
   </div>
 
-  Confirm
+  <div>
+    <button type="button" on:click={handleClick}>Confirm</button>
+  </div>
+
 
   <!-- Completed state start -->
 
