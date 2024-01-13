630ccb8e05c23a522de2b0be566ca3d3c20613ad
baglana <102351104+baglana@users.noreply.github.com>
2024-01-13 14:22:12 +0600

    Add card

--- src/App.svelte
+++ src/App.svelte
@@ -1,41 +1,57 @@
 <script lang="ts">
-  let name = '';
-  let number = '';
-  let month = 1;
-  let year = 24;
+  import Card from './lib/Card.svelte';
+
+  type Card = {
+    number: string;
+    name: string;
+    month: string;
+    year: string
+    cvc: string;
+  };
+
+  let data: Card = {
+    number: '0000000000000000',
+    name: 'Jane Appleseed',
+    month: '00',
+    year: '00',
+    cvc: '000'
+  };
   
   function handleClick() {
     console.log("hello");
   }
+
+  function handleInput(e: Event) {
+    const target = e.target as HTMLInputElement;
+    if (target) {
+      data[target.name as keyof Card] = target.value;
+    }
+  }
 </script>
 
-0000 0000 0000 0000
-Jane Appleseed
-00/00
-
-000
+<Card {...data}/>
 
 <form action="https://httpbin.org/post" method="post">
   <div>
     <label for="name">Cardholder Name</label>
-    <input type="text" id="name" name="name" placeholder="e.g. Jane Appleseed" bind:value={name}/>
+    <input type="text" id="name" name="name" placeholder="e.g. Jane Appleseed" on:input={handleInput}/>
   </div>
 
   <div>
     <label for="number">Card Number</label>
-    <input type="text" id="number" name="number" placeholder="e.g. 1234 5678 9123 0000" bind:value={number}/>
+    <input type="text" id="number" name="number" placeholder="e.g. 1234 5678 9123 0000" on:input={handleInput}/>
   </div>
 
   <div>
     <label>Exp. Date (MM/YY)
-      <input type="number" id="month" name="month" placeholder="MM" bind:value={month}/>
-      <input type="number" id="year" name="year" placeholder="YY" bind:value={year}/>
+      <input type="number" id="month" name="month" placeholder="MM" on:input={handleInput}/>
+      <input type="number" id="year" name="year" placeholder="YY" on:input={handleInput}>
     </label>
   </div>
 
   <div>
     <label for="cvc">CVC</label>
-    <input type="number" id="cvc" name="cvc" placeholder="e.g. 123">
+    <input type="number" id="cvc" name="cvc" placeholder="e.g. 123" on:input={handleInput}>
   </div>
 
   <div>
---
+++ src/lib/Card.svelte
@@ -0,0 +1,38 @@
+<script>
+  export let name;
+  export let number;
+  export let month;
+  export let year;
+  export let cvc;
+</script>
+
+<article class="front">
+  <p>{number}</p>
+
+  <span>{name}</span>
+
+  <span>{month}/{year}</span>
+</article>
+
+<article class="back">
+  <p>{cvc}</p>
+</article>
+
+<style>
+  article {
+      width: 27.9375rem;
+      /*height: 15.3125rem;*/
+      flex-shrink: 0;
+      border-radius: 0.625rem;
+
+      &.front {
+          background: linear-gradient(164deg, #6348FE 4.74%, #610595 88.83%);
+          color: #fff;
+      }
+
+      &.back {
+          background: linear-gradient(169deg, #FFF 5%, #D2D3D9 91.69%);
+          color: #000;
+      }
+  }
+</style>
