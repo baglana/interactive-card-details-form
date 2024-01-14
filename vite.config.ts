ca67f268465a5496fb9d55eccd2cbf36ddbefbd6
baglana <102351104+baglana@users.noreply.github.com>
2024-01-14 23:39:08 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: "https://baglana.github.io/interactive-card-details-form"
 })
