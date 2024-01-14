b4e28f82b4555e337883990bb6e04747c415f24b
baglana <102351104+baglana@users.noreply.github.com>
2024-01-14 23:11:44 +0600

    Update form

--- src/App.svelte
+++ src/App.svelte
@@ -34,27 +34,27 @@
 <Card {...data}/>
 
 {#if !confirmed}
-  <form action="https://httpbin.org/post" method="post">
+  <form novalidate action="https://httpbin.org/post" method="post">
     <div>
       <label for="name">Cardholder Name</label>
-      <input type="text" id="name" name="name" placeholder="e.g. Jane Appleseed" on:input={handleInput}/>
+      <input type="text" id="name" name="name" autocomplete="cc-name" placeholder="e.g. Jane Appleseed" on:input={handleInput}/>
     </div>
 
     <div>
       <label for="number">Card Number</label>
-      <input type="text" id="number" name="number" placeholder="e.g. 1234 5678 9123 0000" on:input={handleInput}/>
+      <input type="text" id="number" name="number" inputmode="numeric" autocomplete="cc-number" placeholder="e.g. 1234 5678 9123 0000" maxlength="16" on:input={handleInput}/>
     </div>
 
     <div>
       <label>Exp. Date (MM/YY)
-        <input type="number" id="month" name="month" placeholder="MM" on:input={handleInput}/>
-        <input type="number" id="year" name="year" placeholder="YY" on:input={handleInput}>
+        <input type="number" id="month" name="month" autocomplete="cc-exp-month" placeholder="MM" min="1" max="12" on:input={handleInput}/>
+        <input type="number" id="year" name="year" autocomplete="cc-exp-year" placeholder="YY" min="24" max="99" on:input={handleInput}>
       </label>
     </div>
 
     <div>
       <label for="cvc">CVC</label>
-      <input type="password" id="cvc" name="cvc" placeholder="e.g. 123" on:input={handleInput}>
+      <input type="password" id="cvc" name="cvc" inputmode="numeric" autocomplete="cc-csc" placeholder="e.g. 123" maxlength="3" on:input={handleInput}>
     </div>
 
     <div>
@@ -82,3 +82,26 @@
   Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>.
   Coded by <a href="#">Your Name Here</a>.
 </div>
+
+<style>
+    form {
+        max-width: 23.8125rem;
+        display: flex;
+        flex-direction: column;
+    }
+
+    input {
+        /*width: 100%;*/
+        /*border-radius: 0.5rem;*/
+        border: 1px solid var(--Light-Grey, #DFDEE0);
+        /*background: var(--White, #FFF);*/
+
+        &:focus {
+            border: 1px solid var(--Gradient, #6348FE);
+        }
+        &:invalid {
+            border: 1px solid var(--Red, #FF5050);
+        }
+  }
+
+</style>
